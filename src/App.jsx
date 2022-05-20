76ed0947d6286f6a04ae94ea09dd3af200ab7af1
Carlos <carlos@MacBook-Pro-de-Carlos.local>
2022-05-19 23:47:24 -0500

    primer comentario

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button } from 'react-bootstrap';
+import './App.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+  
+const App = () => {
+  const [peliculas, setPelicuas] = useState(0)
+  return (
+    <div className="App">
+      
+      <h1>Apps de Peliculas</h1>
+
+
+ 
+    </div>
+  )
+}
+export default App
